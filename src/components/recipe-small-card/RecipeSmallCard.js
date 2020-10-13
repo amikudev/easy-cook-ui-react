912757e2e7cacfb1a53678ff40acf3b6ed40df3f
Amit Kumar <amitgarg.cse@gmail.com>
2020-10-13 17:49:32 +0530

    Adding cursor pointer to RecipeSmallCard

--- src/components/recipe-small-card/RecipeSmallCard.js
+++ src/components/recipe-small-card/RecipeSmallCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './RecipeSmallCard.css'
 
 export default class RecipeSmallCard extends React.Component {
     render(props) {
         return (
-            <div onClick={() => this.props.recipeClickHandler(this.props.recipe._id)}>{this.props.recipe.title}</div>
+            <div className="recipe-small-card" onClick={() => this.props.recipeClickHandler(this.props.recipe._id)}>
+                {this.props.recipe.title}
+            </div>
         )
     }
 }
