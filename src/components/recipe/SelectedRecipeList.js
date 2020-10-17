788878ff28b54bd5c3a7ae234a814939e2080905
Amit Kumar <amitgarg.cse@gmail.com>
2020-10-17 17:01:44 +0530

    Improving margins and look and feel of page.

--- src/components/recipe/SelectedRecipeList.js
+++ src/components/recipe/SelectedRecipeList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Recipe from "./Recipe";
+import classes from "./SelectedRecipeList.module.scss";
 
 export default class SelectedRecipeList extends React.Component {
   render() {
     return (
-      <div>
+      <div className={classes.selectedRecipes}>
         {this.props.selectedRecipeList.map((recipe) => (
           <Recipe recipe={recipe} />
         ))}
