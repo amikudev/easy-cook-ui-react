e7fa0f926b8dbb5fe476df3f5ae78a10826f4ffd
Amit Kumar <amitgarg.cse@gmail.com>
2020-10-18 20:08:35 +0530

    Reduce rendering time

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default class App extends React.Component {
             allRecipes: recipes,
             selectedRecipeList: [recipes[0]],
           }),
-        500
+        0
       );
     }
 
