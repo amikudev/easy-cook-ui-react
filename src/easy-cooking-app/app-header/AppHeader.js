829c244e0c9a94f0849fcf50080aea8559177346
Amit Kumar <amitgarg.cse@gmail.com>
2020-10-24 19:21:55 +0530

    Prop types

--- src/easy-cooking-app/app-header/AppHeader.js
+++ src/easy-cooking-app/app-header/AppHeader.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import classes from "./AppHeader.module.scss";
 
 export default class AppHeader extends React.Component {
@@ -10,3 +11,7 @@ export default class AppHeader extends React.Component {
     );
   }
 }
+
+AppHeader.propTypes = {
+  appName: PropTypes.string,
+};
