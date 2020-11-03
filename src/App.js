afcac0fe7dbe3d9293cabfdb1b7b70e7dd142d7d
Amit Kumar <amitgarg.cse@gmail.com>
2020-11-03 21:10:35 +0530

    Added React Router and Login Component Placeholder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import EasyCookingApp from "./easy-cooking-app/EasyCookingApp";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./components/Login";
 
 export default class App extends React.Component {
   render = () => {
-    return <EasyCookingApp></EasyCookingApp>;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <EasyCookingApp />
+          </Route>
+        </Switch>
+      </Router>
+    );
   };
 }
