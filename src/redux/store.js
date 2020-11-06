2c7af8eec701f871b37379707a787d5b439dcb3e
Amit Kumar <amitgarg.cse@gmail.com>
2020-11-06 18:24:34 +0530

    redux-localstorage trial

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { compose, createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 import reduxThunk from "redux-thunk";
+import persistState from "redux-localstorage";
 
-export default createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(reduxThunk))
+const enhancer = compose(
+  composeWithDevTools(applyMiddleware(reduxThunk)),
+  persistState()
 );
+
+export default createStore(rootReducer, enhancer);
