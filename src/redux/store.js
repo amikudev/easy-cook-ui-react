f15d7bc2cce96dae658d8b89271dddea1e23a40a
Amit Kumar <amitgarg.cse@gmail.com>
2020-11-06 18:25:01 +0530

    Revert "redux-localstorage trial"
    This reverts commit 2c7af8eec701f871b37379707a787d5b439dcb3e.

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,9 @@
-import { compose, createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./reducers";
 import reduxThunk from "redux-thunk";
-import persistState from "redux-localstorage";
 
-const enhancer = compose(
-  composeWithDevTools(applyMiddleware(reduxThunk)),
-  persistState()
+export default createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxThunk))
 );
-
-export default createStore(rootReducer, enhancer);
