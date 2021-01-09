198a67752bb353c8f472029cc19a890f141b43eb
Amit Kumar <amitgarg.cse@gmail.com>
2021-01-09 14:54:50 +0530

    child_process for running npm run cron as a child process of build

--- cypress/runner.js
+++ cypress/runner.js
@@ -1,5 +1,43 @@
 import cron from "node-cron";
+import { exec } from "child_process";
 
-cron.schedule("* * * * * *", () => {
+// cron.schedule("* * * * * *", () => {
+//   console.log("Running a task every second");
+//
+//   exec("dir", (err, stdout, stderr) => {
+//     if (err) {
+//       //some err occurred
+//       console.error(err);
+//     } else {
+//       // the *entire* stdout and stderr (buffered)
+//       console.log(`stdout: ${stdout}`);
+//       console.log(`stderr: ${stderr}`);
+//     }
+//   });
+// });
+
+exec("npm run cypress:run", (err, stdout, stderr) => {
+  if (err) {
+    //some err occurred
+    console.error(err);
+  } else {
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  }
+});
+
+cron.schedule("* * * * *", () => {
   console.log("Running a task every second");
+
+  // exec("npm run cypress:run", (err, stdout, stderr) => {
+  //   if (err) {
+  //     //some err occurred
+  //     console.error(err);
+  //   } else {
+  //     // the *entire* stdout and stderr (buffered)
+  //     console.log(`stdout: ${stdout}`);
+  //     console.log(`stderr: ${stderr}`);
+  //   }
+  // });
 });
